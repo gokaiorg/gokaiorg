bd1a2d2e7c600495372e8826e1ef0b8d7eff6b26
Jeremy <jeremy.douchamps@emerya.fr>
2020-12-06 15:39:44 +0700

    Update main menu

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -11,6 +11,16 @@ const Nav = () => (
         </a>
       </li>
       <li>
+        <a href="https://app.zerion.io/0x41646fb881acd56bad144b343a3525b6907bc86b/overview" target="_blank">
+          Fund
+        </a>
+      </li>
+      <li>
+        <a href="https://snapshot.page/#/gokai.eth" target="_blank">
+          Vote
+        </a>
+      </li>
+      <li>
         <Link to="/contact">Contact</Link>
       </li>
     </ul>
